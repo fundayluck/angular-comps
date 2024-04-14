1379a569fa45b46a952323a3c13ef1552a6d68a4
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-14 18:04:53 +0700

    adding view elements home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, ElementsHomeComponent],
 })
 export class AppComponent {
   title = 'comps';
