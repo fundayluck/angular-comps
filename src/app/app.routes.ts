272a15840259c38e661c645b6ce854b8e9912f10
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-14 18:48:27 +0700

    adding home and not Found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
 import { AppComponent } from './app.component';
 import { CollectionsHomeComponent } from './collections/collections-home/collections-home.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
   { path: 'elements', component: ElementsHomeComponent },
   { path: 'collections', component: CollectionsHomeComponent },
+  { path: '**', component: NotFoundComponent },
 ];
