85e17ba1d85de7cfeb310c7d4449ca9b0f6b21d2
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-15 07:37:49 +0700

    lazy load page elements and collections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,22 @@
 import { Routes } from '@angular/router';
-import { CollectionsHomeComponent } from './collections/collections-home/collections-home.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
   {
-    path: '',
+    path: 'elements',
     loadChildren: () =>
       import('./elements/elements-routing.module').then(
         (m) => m.ElementsRoutingModule
       ),
   },
-  { path: 'collections', component: CollectionsHomeComponent },
+  {
+    path: 'collections',
+    loadChildren: () =>
+      import('./collections/collections-routing.module').then(
+        (m) => m.CollectionsRoutingModule
+      ),
+  },
+  { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponent },
 ];
