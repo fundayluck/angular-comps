f7854f76ec9c4a46586cb51ed943d1f805fa0af7
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-15 07:57:25 +0700

    reminder on structural directives

--- src/app/elements/elements-home/elements-home.component.ts
+++ src/app/elements/elements-home/elements-home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { PlaceholderComponent } from '../placeholder/placeholder.component';
 
 @Component({
   selector: 'app-elements-home',
   standalone: true,
-  imports: [],
+  imports: [PlaceholderComponent],
   templateUrl: './elements-home.component.html',
-  styleUrl: './elements-home.component.css'
+  styleUrl: './elements-home.component.css',
 })
-export class ElementsHomeComponent {
-
-}
+export class ElementsHomeComponent {}
