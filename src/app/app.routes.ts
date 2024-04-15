293890d4befe91076077020268a07719394f2de1
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-15 07:20:52 +0700

    implementing lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
-import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
 import { CollectionsHomeComponent } from './collections/collections-home/collections-home.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'elements', component: ElementsHomeComponent },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./elements/elements-routing.module').then(
+        (m) => m.ElementsRoutingModule
+      ),
+  },
   { path: 'collections', component: CollectionsHomeComponent },
   { path: '**', component: NotFoundComponent },
 ];
