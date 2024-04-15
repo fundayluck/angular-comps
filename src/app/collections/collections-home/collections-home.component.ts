b82e68c02afba51095a55c57ee154034fd4c6e00
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-15 09:44:45 +0700

    generating and displaying the table

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { TableComponent } from '../table/table.component';
+import { DividerComponent } from '../../shared/divider/divider.component';
 
 @Component({
   selector: 'app-collections-home',
   standalone: true,
-  imports: [],
+  imports: [TableComponent, DividerComponent],
   templateUrl: './collections-home.component.html',
-  styleUrl: './collections-home.component.css'
+  styleUrl: './collections-home.component.css',
 })
-export class CollectionsHomeComponent {
-
-}
+export class CollectionsHomeComponent {}
