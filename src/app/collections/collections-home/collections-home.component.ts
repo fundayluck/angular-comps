8674b28f120176db2005da2d8c13dd963e89afb5
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-16 21:26:06 +0700

    a reusable tabs component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TableComponent } from '../table/table.component';
 import { DividerComponent } from '../../shared/divider/divider.component';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { TabsComponent } from '../tabs/tabs.component';
 
 @Component({
   selector: 'app-collections-home',
@@ -12,6 +13,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
     RouterLinkActive,
     DividerComponent,
     TableComponent,
+    TabsComponent,
   ],
   templateUrl: './collections-home.component.html',
   styleUrl: './collections-home.component.css',
