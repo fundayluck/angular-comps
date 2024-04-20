f027ad2598eed0bfeb75a878f4d5b83ea142c9cc
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-20 21:55:53 +0700

    building an accordion

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ModalComponent } from '../modal/modal.component';
 import { DividerComponent } from '../../shared/divider/divider.component';
 import { CommonModule } from '@angular/common';
+import { AccordionComponent } from '../accordion/accordion.component';
 
 @Component({
   selector: 'app-mods-home',
   standalone: true,
-  imports: [ModalComponent, DividerComponent, CommonModule],
+  imports: [ModalComponent, DividerComponent, AccordionComponent, CommonModule],
   templateUrl: './mods-home.component.html',
   styleUrl: './mods-home.component.css',
 })
