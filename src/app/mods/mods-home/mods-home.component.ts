3ac0b1b1bf9b548bffc0370895a225334e294174
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-20 21:20:24 +0700

    opening the modal

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { ModalComponent } from '../modal/modal.component';
 import { DividerComponent } from '../../shared/divider/divider.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mods-home',
   standalone: true,
-  imports: [ModalComponent, DividerComponent],
+  imports: [ModalComponent, DividerComponent, CommonModule],
   templateUrl: './mods-home.component.html',
   styleUrl: './mods-home.component.css',
 })
-export class ModsHomeComponent {}
+export class ModsHomeComponent {
+  modalOpen = false;
+  onClick() {
+    this.modalOpen = !this.modalOpen;
+  }
+}
